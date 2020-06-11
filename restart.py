451904b11ec91e9c008e575a3f87b6037c3996cd
Jarosław <50953962+flame4ost@users.noreply.github.com>
2020-06-11 14:36:49 +0300

    remastered code

--- restart.py
+++ restart.py
@@ -1,23 +1,7 @@
 from modules.essentialSkript import updateSkript,forceUpdate
 
 
-#
-#         ______  ___ _____   ______  _____  ____       ______    ___   ____  ___ ___
-#		 / ___/  /  _]|    \ |      | /   \ |    \     |      |  /  _] /    ||   |   |
-#		(   \_  /  [_ |  D  )|      ||     ||  D  )    |      | /  [_ |  o  || _   _ |
-#		 \__  ||    _]|    / |_|  |_||  O  ||    /     |_|  |_||    _]|     ||  \_/  |
-#		 /  \ ||   [_ |    \   |  |  |     ||    \       |  |  |   [_ |  _  ||   |   |
-#		 \    ||     ||  .  \  |  |  |     ||  .  \      |  |  |     ||  |  ||   |   |
-#		  \___||_____||__|\_|  |__|   \___/ |__|\_|      |__|  |_____||__|__||___|___|
-#
-#
-#       CODE BY: JAY MEHTA & TORY11
-#       DEVELOPER TEAM: SERTOR TEAM
-#       CODE VERSION: 0.1.5.3
-#       
-#       GITHUB TEAM URL: https://github.com/SerTor-TEAM
-#       WANT TO JOIN US? POST YOUR USERNAME HERE: {url}
-#
+
 
 try:
     updateSkript()
