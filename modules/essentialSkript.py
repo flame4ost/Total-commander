45c3079bf511f9a073b67027b342c88e8bc66832
Jarosław <50953962+flame4ost@users.noreply.github.com>
2020-06-11 14:39:08 +0300

    remastered code

--- modules/essentialSkript.py
+++ modules/essentialSkript.py
@@ -4,26 +4,6 @@ from modules.fileSkript import makeFile
 import requests as rq
 
 
-
-#
-#         ______  ___ _____   ______  _____  ____       ______    ___   ____  ___ ___
-#		 / ___/  /  _]|    \ |      | /   \ |    \     |      |  /  _] /    ||   |   |
-#		(   \_  /  [_ |  D  )|      ||     ||  D  )    |      | /  [_ |  o  || _   _ |
-#		 \__  ||    _]|    / |_|  |_||  O  ||    /     |_|  |_||    _]|     ||  \_/  |
-#		 /  \ ||   [_ |    \   |  |  |     ||    \       |  |  |   [_ |  _  ||   |   |
-#		 \    ||     ||  .  \  |  |  |     ||  .  \      |  |  |     ||  |  ||   |   |
-#		  \___||_____||__|\_|  |__|   \___/ |__|\_|      |__|  |_____||__|__||___|___|
-#
-#
-#       CODE BY: JAY MEHTA & TORY11
-#       DEVELOPER TEAM: SERTOR TEAM
-#       CODE VERSION: 0.1.5.3
-#       
-#       GITHUB TEAM URL: https://github.com/SerTor-TEAM
-#       WANT TO JOIN US? POST YOUR USERNAME HERE: {url}
-#
-
-
 def restart():
     python = sys.executable
     os.execl(python, python, * sys.argv)
